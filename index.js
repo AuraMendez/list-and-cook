97a08c65b77969d6baabf30d0b05b5fdb08857d8
Aura Méndez Lecomte <auralecomte@gmail.com>
2020-10-19 14:45:46 +0200

    rest api

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const path = require('path');
+const bodyParser = require('body-parser');
+const recipes = require('./db/recipes.json').recipes;
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use(cors())
+app.use(bodyParser.json());
+
+
+// Rest API 
+app.get('/api/recipes', (req, res) => {
+  res.send(recipes);
+});
+
+app.get('/api/recipes/index/:i', (req, res) => {
+  const recipeByIndex = recipes[req.params.i];
+  res.send(recipeByIndex);
+});
+
+app.get('/api/recipes/id/:id', (req, res) => {
+  const recipeById = recipes.filter(recipe => recipe.id === req.params.id);
+  res.send(recipeById[0]);
+});
+
+app.get('/api/recipes/tag/:tag', (req, res) => {
+  const recipesByTag = recipes.filter(recipe => recipe.tags.includes(req.params.tag));
+  res.send(recipesByTag);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
